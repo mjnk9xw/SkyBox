d7ddc3aaa50ee969cc9d6eb68e201b113707892c
mjnk9xw <minhnguyen998vp@gmail.com>
2020-03-31 12:36:06 +0700

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,313 @@
+﻿#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+#pragma comment(lib, "winmm.lib")						// Added for timeGetTime()
+#include "main.h"
+#include "camera.h"										// This includes our camera class header
+#include <GL/glut.h>
+
+bool  g_bFullScreen = TRUE;								// Set full screen as default
+HWND  g_hWnd;											// This is the handle for the window
+RECT  g_rRect;											// This holds the window dimensions
+HDC   g_hDC;											// General HDC - (handle to device context)
+HGLRC g_hRC;											// General OpenGL_DC - Our Rendering Context for OpenGL
+HINSTANCE g_hInstance;									// This holds the global hInstance for UnregisterClass() in DeInit()
+float g_DT;												// This stores the elapsed time between the current and last frame
+
+#define SPEED	0.03f									// This is how fast our camera moves
+
+// These defines correspond to an index within the g_Texture[] array.
+// We will use these defines to bind the appropriate texture to the
+// right side of the box.
+
+#define BACK_ID		0									// The texture ID for the back side of the cube
+#define FRONT_ID	1									// The texture ID for the front side of the cube
+#define BOTTOM_ID	2									// The texture ID for the bottom side of the cube
+#define TOP_ID		3									// The texture ID for the top side of the cube
+#define LEFT_ID		4									// The texture ID for the left side of the cube
+#define RIGHT_ID	5									// The texture ID for the right side of the cube
+
+UINT g_Texture[MAX_TEXTURES] = {0};						// This holds the texture info, referenced by an ID
+
+CCamera g_Camera;										// This is our global camera object
+
+///////////////////////////////// CREATE SKY BOX \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*
+/////
+/////	This creates a box centered around X Y Z.  Instead of colors at
+/////   each vertices there is texture maps for each side of the box to 
+/////   create the illusion of the sky box.  You can set the width, height and length
+/////   of the sky box to create different perspectives for different sky box textures.
+/////
+///////////////////////////////// CREATE SKY BOX \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\*
+void initLight() {
+	//// light
+	//GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 10.0 };
+	//GLfloat mat_shininess[] = { 10.0 };
+	//GLfloat light_position[] = { 20.0, 20.0, -1.0, 1.0 };
+	//glShadeModel(GL_SMOOTH);
+
+	//glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+	//glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+	//glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+	//GLfloat global_ambient[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	//glLightModelfv(GL_LIGHT_MODEL_AMBIENT, global_ambient);
+
+	//glEnable(GL_LIGHTING);
+	//glEnable(GL_LIGHT0);
+
+	/*glClearColor(0.0, 0.0, 0.0, 1.0); 
+	glClearDepth(1.0);
+
+	glEnable(GL_LIGHT0); 
+	glEnable(GL_LIGHTING); 
+	glEnable(GL_DEPTH_TEST);*/
+
+}
+
+void spotlight(void)
+{
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	initLight();
+	GLfloat light_position[] = { 20.0, 20.0, 1.0, 1.0 };
+	glLightfv(GL_LIGHT1, GL_SPOT_DIRECTION, light_position);
+	glLightf(GL_LIGHT1, GL_SPOT_EXPONENT, 15.0f);
+	glLightf(GL_LIGHT1, GL_SPOT_CUTOFF, 180.0);
+	
+	glColor3f(1.0, 1.0, 1.0);
+	glutSolidSphere(2.0, 200, 160);
+	glFlush();
+
+}
+void CreateSkyBox(float x, float y, float z, float width, float height, float length)
+{
+
+	//drawSphere();
+	glBindTexture(GL_TEXTURE_2D, g_Texture[BACK_ID]);
+	
+
+	// This centers the sky box around (x, y, z)
+	x = x - width  / 2;
+	y = y - height / 2;
+	z = z - length / 2;
+
+		// Start drawing the side as a QUAD
+	glBegin(GL_QUADS);		
+		
+		// Assign the texture coordinates and vertices for the BACK Side
+
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x + width, y,			z);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x + width, y + height, z); 
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x,			y + height, z);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x,			y,			z);
+
+		
+		
+	glEnd();
+
+	// Bind the FRONT texture of the sky map to the FRONT side of the box
+	glBindTexture(GL_TEXTURE_2D, g_Texture[FRONT_ID]);
+
+	// Start drawing the side as a QUAD
+	glBegin(GL_QUADS);	
+	
+		// mặt trước
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x,			y,			z + length);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x,			y + height, z + length);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x + width, y + height, z + length); 
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x + width, y,			z + length);
+
+		spotlight();
+		
+	glEnd();
+
+	// Bind the BOTTOM texture of the sky map to the BOTTOM side of the box
+	glBindTexture(GL_TEXTURE_2D, g_Texture[BOTTOM_ID]);
+
+	// Start drawing the side as a QUAD
+	glBegin(GL_QUADS);		
+	
+		// Assign the texture coordinates and vertices for the BOTTOM Side
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x,			y,			z);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x,			y,			z + length);
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x + width, y,			z + length); 
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x + width, y,			z);
+	glEnd();
+
+	// Bind the TOP texture of the sky map to the TOP side of the box
+	glBindTexture(GL_TEXTURE_2D, g_Texture[TOP_ID]);
+	
+	// Start drawing the side as a QUAD
+	glBegin(GL_QUADS);		
+		
+		// Assign the texture coordinates and vertices for the TOP Side
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x + width, y + height, z);
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x + width, y + height, z + length); 
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x,			y + height,	z + length);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x,			y + height,	z);
+		
+	glEnd();
+
+	// Bind the LEFT texture of the sky map to the LEFT side of the box
+	glBindTexture(GL_TEXTURE_2D, g_Texture[LEFT_ID]);
+	
+	// Start drawing the side as a QUAD
+	glBegin(GL_QUADS);		
+		
+		// Assign the texture coordinates and vertices for the LEFT Side
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x,			y + height,	z);	
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x,			y + height,	z + length); 
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x,			y,			z + length);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x,			y,			z);		
+		
+	glEnd();
+
+	// Bind the RIGHT texture of the sky map to the RIGHT side of the box
+	glBindTexture(GL_TEXTURE_2D, g_Texture[RIGHT_ID]);
+
+	// Start drawing the side as a QUAD
+	glBegin(GL_QUADS);		
+
+		// Assign the texture coordinates and vertices for the RIGHT Side
+		glTexCoord2f(0.0f, 0.0f); glVertex3f(x + width, y,			z);
+		glTexCoord2f(1.0f, 0.0f); glVertex3f(x + width, y,			z + length);
+		glTexCoord2f(1.0f, 1.0f); glVertex3f(x + width, y + height,	z + length); 
+		glTexCoord2f(0.0f, 1.0f); glVertex3f(x + width, y + height,	z);
+	glEnd();
+
+}
+
+
+void Init(HWND hWnd)
+{
+	g_hWnd = hWnd;										// Assign the window handle to a global window handle
+	GetClientRect(g_hWnd, &g_rRect);					// Assign the windows rectangle to a global RECT
+	InitializeOpenGL(g_rRect.right, g_rRect.bottom);	// Init OpenGL with the global rect
+
+	CreateTexture(g_Texture[BACK_ID], "Back.bmp");
+	CreateTexture(g_Texture[FRONT_ID], "Front.bmp");
+	CreateTexture(g_Texture[BOTTOM_ID], "Bottom.bmp");
+	CreateTexture(g_Texture[TOP_ID], "Top.bmp");
+	CreateTexture(g_Texture[LEFT_ID], "Left.bmp");
+	CreateTexture(g_Texture[RIGHT_ID], "Right.bmp");
+
+	// Give our camera a decent starting point in the world
+	g_Camera.PositionCamera( 0, 0, 0,	0, 0, 1,	0, 1, 0);
+}
+
+
+bool AnimateNextFrame(int desiredFrameRate)
+{
+	static float lastTime = GetTickCount() * 0.001f;
+	static float elapsedTime = 0.0f;
+
+	float currentTime = GetTickCount() * 0.001f; // Get the time (milliseconds = seconds * .001)
+	float deltaTime = currentTime - lastTime; // Get the slice of time
+	float desiredFPS = 1.0f / desiredFrameRate; // Store 1 / desiredFrameRate
+
+	elapsedTime += deltaTime; // Add to the elapsed time
+	lastTime = currentTime; // Update lastTime
+
+	// Check if the time since we last checked is greater than our desiredFPS
+	if( elapsedTime > desiredFPS )
+	{
+		g_DT = desiredFPS;
+		elapsedTime -= desiredFPS; // Adjust our elapsed time
+
+		// Return true, to animate the next frame of animation
+		return true;
+	}
+
+	// We don't animate right now.
+	return false;
+}
+
+
+WPARAM MainLoop()
+{
+	MSG msg;
+
+	while(1)											// Do our infinite loop
+	{													// Check if there was a message
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) 
+		{ 
+			if(msg.message == WM_QUIT)					// If the message wasn't to quit
+				break;
+			TranslateMessage(&msg);						// Find out what the message does
+			DispatchMessage(&msg);						// Execute the message
+		}
+		else											// if there wasn't a message
+		{ 		
+			if(AnimateNextFrame(60))					// Make sure we only animate 60 FPS
+			{
+				g_Camera.Update();						// Update the camera data			
+
+				RenderScene();							// Render the scene every frame
+			}
+			else
+			{
+				Sleep(1);								// Let other processes work
+			}
+		} 
+	}
+
+	DeInit();											// Clean up and free all allocated memory
+
+	return(msg.wParam);									// Return from the program
+}
+
+
+void RenderScene() 
+{
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);	// Clear The Screen And The Depth Buffer
+	glLoadIdentity();									// Reset The matrix
+
+	g_Camera.Look();
+
+	// Draw the sky box centered at (0, 0, 0)
+	CreateSkyBox(0, 0, 0, 400, 200, 400);
+
+	SwapBuffers(g_hDC);									// Swap the backbuffers to the foreground
+
+	//spotlight();
+}
+
+LRESULT CALLBACK WinProc(HWND hWnd,UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    LONG    lRet = 0; 
+    PAINTSTRUCT    ps;
+
+    switch (uMsg)
+	{ 
+    case WM_SIZE:										// If the window is resized
+		if(!g_bFullScreen)								// Do this only if we are NOT in full screen
+		{
+			SizeOpenGLScreen(LOWORD(lParam),HIWORD(lParam));// LoWord=Width, HiWord=Height
+			GetClientRect(hWnd, &g_rRect);					// Get the window rectangle
+		}
+        break; 
+ 
+	case WM_PAINT:										// If we need to repaint the scene
+		BeginPaint(hWnd, &ps);							// Init the paint struct		
+		EndPaint(hWnd, &ps);							// EndPaint, Clean up
+		break;
+
+	case WM_KEYDOWN:
+		switch(wParam) {								// Check if we hit a key
+			case VK_ESCAPE:								// If we hit the escape key
+				PostQuitMessage(0);						// Send a QUIT message to the window
+				break;
+		}
+		break;
+ 
+    case WM_DESTROY:									// If the window is destroyed
+        PostQuitMessage(0);								// Send a QUIT Message to the window
+        break; 
+     
+    default:											// Return by default
+        lRet = DefWindowProc (hWnd, uMsg, wParam, lParam); 
+        break; 
+    } 
+ 
+    return lRet;										// Return by default
+}
